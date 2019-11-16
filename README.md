780a6b6a8c7b3ee6f8d4ccabc8ef9b819bf22708
Rafael Costa <rafaelsevla@gmail.com>
2019-11-16 17:22:01 -0300

    fix

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
 - Escolha uma peça
+
 - Escolha o dia
+
 - Escolha o horario
+
 - Escolha uma poltrona (letra e número)
+
 - Se pontrona já foi reservada imprima na tela "Lugar ocupado"
+
 - Insira sua idade
+
 - Professor de rede publica V ou F
+
 - Estudante V ou F
+
 - Se estudante for V, insira seu RA
+
+- Se RA (tabelinha com RA dos alunos carentes) for igual a V e dia for igual a TERÇA, o ingreço preço será reduzido a 100%, se não preço normal
+
 - Se Idade for entre 02 e 12 ingreço preço será reduzido em 50%, se não preço normal
-- Se Professor publico for igual a V o ingreço preço será reduzido em 50%, se não preço normal
-- Se Estudante for igual a V o ingreço preço será reduzido em 50%, se não preço normal
+
 - Se Idade for maior ou igual a 60, o ingreço preço será reduzido em 50%, se não preço normal
-iiiiiiiiiiiiiiiiiiiiiiiiii
+
+- Se Professor publico for igual a V o ingreço preço será reduzido em 50%, se não preço normal
+
+- Se Estudante for igual a V o ingreço preço será reduzido em 50%, se não preço normal
+
+- Confirmar ingresso
+
+- Se ingresso confirmado imprimir o ingresso
+
+- se não confirmado volte para o começo
--- pim4.c
+++ pim4.c
@@ -137,25 +137,31 @@ void confirmTicketFreeEntry(char *hour, char *teather, char *day)
 
 void verifyMidEntry(char *hour, char *teather, char *day)
 {
+  printf("%s", day);
+
   int age, ra;
   int isFree = 0;
   char teacher, student, hasRa;
 
-  printf("Você tem R.A.? Digite V se sim.\n");
-  scanf("%s", &hasRa);
-
-  if (hasRa == 'V' || hasRa == 'v')
+  if (itsFreeDay >= 1)
   {
-    printf("Digite seu R.A.\n");
-    scanf("%i", &ra);
-    if (ra == 1929779 || ra == 1980345 || ra == 1981881 & itsFreeDay >= 1)
+
+    printf("Você tem R.A.? Digite V se sim.\n");
+    scanf("%s", &hasRa);
+
+    if (hasRa == 'V' || hasRa == 'v')
     {
-      isFree = 1;
-      confirmTicketFreeEntry(hour, teather, day);
+      printf("Digite seu R.A.\n");
+      scanf("%i", &ra);
+      if (ra == 1929779 || ra == 1980345 || ra == 1981881)
+      {
+        isFree = 1;
+        confirmTicketFreeEntry(hour, teather, day);
+      }
     }
   }
 
-  if (isFree == 0)
+  if (isFree == 0 || hasRa != 'V' || hasRa != 'v')
   {
     printf("Qual sua idade?\n");
     scanf("%i", &age);
@@ -285,7 +291,7 @@ void choiceHour(char *teather, char *day)
 
 void choiceDay(char *teather)
 {
-  int day;
+  int choiceDay;
   printf("\t-----------------------------------------------\n");
   printf("\t Teatro: %s \n", teather);
   printf("\t Comprar bilhete para qual dia? \n\n");
@@ -298,8 +304,8 @@ void choiceDay(char *teather)
   printf("\t [7] Domingo \n");
   printf("\t-----------------------------------------------\n\t");
 
-  scanf("%i", &day);
-  switch (day)
+  scanf("%i", &choiceDay);
+  switch (choiceDay)
   {
   case 1:
     choiceHour(teather, "Segunda");
